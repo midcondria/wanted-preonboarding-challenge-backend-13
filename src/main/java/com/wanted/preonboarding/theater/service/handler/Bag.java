5493b39c02d0e1343347c716fb251584b9932b61
midcondria <hyukkind@naver.com>
2023-09-03 19:18:21 +0900

    refactor/Bag 리팩토링 1. 롬복으로 게터 생성 2. 줄 정리 및 빌더패턴 적용

--- src/main/java/com/wanted/preonboarding/theater/service/handler/Bag.java
+++ src/main/java/com/wanted/preonboarding/theater/service/handler/Bag.java
@@ -1,31 +1,41 @@
 package com.wanted.preonboarding.theater.service.handler;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class Bag {
     private Long amount;
     private final Invitation invitation;
     private Ticket ticket;
 
-    public Bag(long amount){
-        this(null, amount);
-    }
+    @Builder
     public Bag(Invitation invitation, long amount){
         this.invitation = invitation;
         this.amount = amount;
     }
 
+    public Bag(long amount){
+        this(null, amount);
+    }
+
     public boolean hasInvitation() {
         return invitation != null;
     }
+
     public boolean hasTicket() {
         return ticket != null;
     }
+
     public void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
-    public void minusAmount(long amount) {
-        this.amount -= amount;
-    }
+
     public void plusAmount(long amount) {
         this.amount += amount;
+    }
+
+    public void minusAmount(long amount) {
+        this.amount -= amount;
     }
 }
